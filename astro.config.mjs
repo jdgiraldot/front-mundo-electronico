be648f46b6738d3343cd2144abe45d50bea45317
Jhonatan Giraldo <jdgiraldo@sistecredito.com>
2026-02-14 08:27:16 -0500

    Corrección configuración Deploy en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://jdgiraldot.github.io',
+    base: 'front-mundo-electronico',
     output: 'static',
     devToolbar: { enabled: false },
-    site: 'https://astronaut.github.io',
-    base: 'mi-repo'
 });
